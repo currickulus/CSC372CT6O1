3e7dd144e189d11ef442e9f7e7696e0cfaa5afdc
Thomas Dewing <thomas.dewing@gmail.com>
2024-11-19 13:48:15 -0800

    Update Sorter.java

--- Sorter.java
+++ Sorter.java
@@ -7,7 +7,7 @@ public class Sorter {//sorts the list from the index position from either the
         for (int i = 0; i < list.size() - 1; i++) {
             int minIndex = i;
             for (int j = i + 1; j < list.size(); j++) {
-                if (comparator.compare(list.get(j), list.get(minIndex)) > 0) {
+                if (comparator.compare(list.get(j), list.get(minIndex)) < 0) {
                     minIndex = j;
 
                 }
